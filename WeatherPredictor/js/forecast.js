faec496095f29f0f14e66341f39a47344c7899e3
Ana <anapandey@hotmail.com>
2017-09-27 15:10:59 -0700

    added comments, beutified code

--- WeatherPredictor/js/forecast.js
+++ WeatherPredictor/js/forecast.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
     var unit="&units=metric";
     var apiKey="&APPID=bd3f7455d381bcf20533ab474c8cbee5";
+    
 //geolocation api call for city & country names
     $.getJSON('http://ip-api.com/json/?callback=?', function(data) {
         var mycity = data.city;
         var mycountry=data.country;
         getWeather(mycity, mycountry);
     });
+    
 // getting weather forecast data from openweathermap, ajax call    
     function getWeather(mycity, mycountry) {
         var apiURL = "http://api.openweathermap.org/data/2.5/weather?q=" + mycity + "," + mycountry + "" + unit + "" + apiKey;
@@ -17,6 +19,7 @@ $(document).ready(function(){
                 $('#cityName').html(mycity + " ," +mycountry);
                 $('#forecastText').html(data.weather[0].main + " , forecast for " + data.weather[0].description);
                 $('#temperature').html(data.main.temp+" &deg;C");
+                
  // converting default temperature unit to Fahrenheit & Celsius               
                 $('#fahrenheitButton').click(function(){
                     $('#temperature').empty().hide().append(Math.round(data.main.temp)*9/5+32 +" &deg;F").fadeIn(800);
@@ -27,6 +30,7 @@ $(document).ready(function(){
             }
         });
     }
+    
  // Getting weather data for cities entered in searchbox, ajax call   
     $('#button').click(function(){
        var city=$('#city').val();
@@ -41,6 +45,7 @@ $(document).ready(function(){
                    $('#cityName').empty().hide().append(data.name+ " , "+data.sys.country).fadeIn(800);
                    $('#forecastText').empty().hide().append(data.weather[0].main + " , forecast for " + data.weather[0].description).fadeIn(800);
                    $('#temperature').empty().hide().append(data.main.temp +" &deg;C").fadeIn(800);
+                   
    //converting data into Fahrenheit  & Celsius                
                    $('#fahrenheitButton').click(function(){
                        $('#temperature').empty().hide().append(Math.round(data.main.temp)*9/5+18 +" &deg;F").fadeIn(800);
